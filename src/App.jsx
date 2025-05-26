6950ce9a54bd8e857a5033682a310b6dd894765e
alfi <david.karic98@gmail.com>
2025-05-26 17:19:02 +0200

    Removed the old slider buttons and added new one. Removed dependency for old one.

--- src/App.jsx
+++ src/App.jsx
@@ -1,112 +1,122 @@
 
 import Footer from './Footer.jsx'
 import Card from './Card.jsx'
+import Buttons from './Buttons.jsx'
 import * as data from './moduleData.js'
-import {MdChevronLeft, MdChevronRight} from 'react-icons/md'
 
 function App() {
 
   const slideLeft = (e) => {
-    var slider = e.currentTarget.parentElement.querySelector('.slajder')
+    var slider = e.currentTarget.parentElement.parentElement.querySelector('div').querySelector('.slajder')
     slider.scrollLeft -= 500
   }
 
   const slideRight = (e) => {
-    var slider = e.currentTarget.parentElement.querySelector('.slajder')
+    var slider = e.currentTarget.parentElement.parentElement.querySelector('div').querySelector('.slajder')
     slider.scrollLeft += 500
   }
 
   return(
     <>
       
-      <h1 className='text-center mt-18 mb-6'>Mathematische & ingenieurwissenschaftliche Grundlagen</h1>
-      <div className='relative flex items-center w-screen'>
-        <MdChevronLeft size={60} onClick={(e)=>slideLeft(e)} className='opacity-50 hover:opacity-100 cursor-pointer' />
-        <div className='slajder w-full h-full overflow-x-scroll scroll whitespace-nowrap scroll-smooth hide-scrollbar'>
-          {data.category1.map((item)=>(
-            <Card imagePath={item.img} name={item.name} description={item.description}></Card>
-          ))}
+      <div>
+        <h1 className='text-center mt-18 mb-6'>Mathematische & ingenieurwissenschaftliche Grundlagen</h1>
+        <div className='relative flex items-center w-screen'>
+          <div className='slajder w-full h-full overflow-x-scroll scroll whitespace-nowrap scroll-smooth hide-scrollbar'>
+            {data.category1.map((item)=>(
+              <Card imagePath={item.img} name={item.name} description={item.description}></Card>
+            ))}
+          </div>
         </div>
-        <MdChevronRight size={60} onClick={(e)=>slideRight(e)} className='opacity-50 hover:opacity-100 cursor-pointer' />
+        <Buttons></Buttons>
       </div>
 
-      <h1 className='text-center mt-18 mb-6'>Computersysteme und Hardwarenahes</h1>
-      <div className='relative flex items-center w-screen'>
-        <MdChevronLeft size={60} onClick={(e)=>slideLeft(e)} className='opacity-50 hover:opacity-100 cursor-pointer' />
-        <div id='slider1' className='slajder w-full h-full overflow-x-scroll scroll whitespace-nowrap scroll-smooth hide-scrollbar'>
-          {data.category2.map((item)=>(
-            <Card imagePath={item.img} name={item.name} description={item.description}></Card>
-          ))}
+
+
+      <div>
+        <h1 className='text-center mt-18 mb-6'>Computersysteme und Hardwarenahes</h1>
+        <div className='relative flex items-center w-screen'>
+          <div id='slider1' className='slajder w-full h-full overflow-x-scroll scroll whitespace-nowrap scroll-smooth hide-scrollbar'>
+            {data.category2.map((item)=>(
+              <Card imagePath={item.img} name={item.name} description={item.description}></Card>
+            ))}
+          </div>
         </div>
-        <MdChevronRight size={60} onClick={(e)=>slideRight(e)} className='opacity-50 hover:opacity-100 cursor-pointer' />
+        <Buttons></Buttons>
       </div>
 
-      <h1 className='text-center mt-18 mb-6'>Programmierung & Software</h1>
-      <div className='relative flex items-center w-screen'>
-        <MdChevronLeft size={60} onClick={(e)=>slideLeft(e)} className='opacity-50 hover:opacity-100 cursor-pointer' />
-        <div id='slider1' className='slajder w-full h-full overflow-x-scroll scroll whitespace-nowrap scroll-smooth hide-scrollbar'>
-          {data.category3.map((item)=>(
-            <Card imagePath={item.img} name={item.name} description={item.description}></Card>
-          ))}
+
+      <div>
+        <h1 className='text-center mt-18 mb-6'>Programmierung & Software</h1>
+        <div className='relative flex items-center w-screen'>
+          <div id='slider1' className='slajder w-full h-full overflow-x-scroll scroll whitespace-nowrap scroll-smooth hide-scrollbar'>
+            {data.category3.map((item)=>(
+              <Card imagePath={item.img} name={item.name} description={item.description}></Card>
+            ))}
+          </div>
         </div>
-        <MdChevronRight size={60} onClick={(e)=>slideRight(e)} className='opacity-50 hover:opacity-100 cursor-pointer' />
+        <Buttons></Buttons>
       </div>
 
+      <div>
       <h1 className='text-center mt-18 mb-6'>Medieninformatik & Usability</h1>
-      <div className='relative flex items-center w-screen'>
-        <MdChevronLeft size={60} onClick={(e)=>slideLeft(e)} className='opacity-50 hover:opacity-100 cursor-pointer' />
-        <div id='slider1' className='slajder w-full h-full overflow-x-scroll scroll whitespace-nowrap scroll-smooth hide-scrollbar'>
-          {data.category4.map((item)=>(
-            <Card imagePath={item.img} name={item.name} description={item.description}></Card>
-          ))}
+        <div className='relative flex items-center w-screen'>
+          <div id='slider1' className='slajder w-full h-full overflow-x-scroll scroll whitespace-nowrap scroll-smooth hide-scrollbar'>
+            {data.category4.map((item)=>(
+              <Card imagePath={item.img} name={item.name} description={item.description}></Card>
+            ))}
+          </div>
         </div>
-        <MdChevronRight size={60} onClick={(e)=>slideRight(e)} className='opacity-50 hover:opacity-100 cursor-pointer' />
+        <Buttons></Buttons>
       </div>
 
-      <h1 className='text-center mt-18 mb-6'>Kommunikationswissenschaft</h1>
-      <div className='relative flex items-center w-screen'>
-        <MdChevronLeft size={60} onClick={(e)=>slideLeft(e)} className='opacity-50 hover:opacity-100 cursor-pointer' />
-        <div id='slider1' className='slajder w-full h-full overflow-x-scroll scroll whitespace-nowrap scroll-smooth hide-scrollbar'>
-          {data.category5.map((item)=>(
-            <Card imagePath={item.img} name={item.name} description={item.description}></Card>
-          ))}
+      <div>
+        <h1 className='text-center mt-18 mb-6'>Kommunikationswissenschaft</h1>
+        <div className='relative flex items-center w-screen'>
+          <div id='slider1' className='slajder w-full h-full overflow-x-scroll scroll whitespace-nowrap scroll-smooth hide-scrollbar'>
+            {data.category5.map((item)=>(
+              <Card imagePath={item.img} name={item.name} description={item.description}></Card>
+            ))}
+          </div>
         </div>
-        <MdChevronRight size={60} onClick={(e)=>slideRight(e)} className='opacity-50 hover:opacity-100 cursor-pointer' />
+        <Buttons></Buttons>
       </div>
 
-      <h1 className='text-center mt-18 mb-6'>Wissenschaftliche Methoden</h1>
-      <div className='relative flex items-center w-screen'>
-        <MdChevronLeft size={60} onClick={(e)=>slideLeft(e)} className='opacity-50 hover:opacity-100 cursor-pointer' />
-        <div id='slider1' className='slajder w-full h-full overflow-x-scroll scroll whitespace-nowrap scroll-smooth hide-scrollbar'>
-          {data.category6.map((item)=>(
-            <Card imagePath={item.img} name={item.name} description={item.description}></Card>
-          ))}
+      <div>
+        <h1 className='text-center mt-18 mb-6'>Wissenschaftliche Methoden</h1>
+        <div className='relative flex items-center w-screen'>
+          <div id='slider1' className='slajder w-full h-full overflow-x-scroll scroll whitespace-nowrap scroll-smooth hide-scrollbar'>
+            {data.category6.map((item)=>(
+              <Card imagePath={item.img} name={item.name} description={item.description}></Card>
+            ))}
+          </div>
         </div>
-        <MdChevronRight size={60} onClick={(e)=>slideRight(e)} className='opacity-50 hover:opacity-100 cursor-pointer' />
+        <Buttons></Buttons>
       </div>
 
-      <h1 className='text-center mt-18 mb-6'>Interdisziplinäre Projekte</h1>
-      <div className='relative flex items-center w-screen'>
-        <MdChevronLeft size={60} onClick={(e)=>slideLeft(e)} className='opacity-50 hover:opacity-100 cursor-pointer' />
-        <div id='slider1' className='slajder w-full h-full overflow-x-scroll scroll whitespace-nowrap scroll-smooth hide-scrollbar'>
-          {data.category7.map((item)=>(
-            <Card imagePath={item.img} name={item.name} description={item.description}></Card>
-          ))}
+      <div>
+        <h1 className='text-center mt-18 mb-6'>Interdisziplinäre Projekte</h1>
+        <div className='relative flex items-center w-screen'>
+          <div id='slider1' className='slajder w-full h-full overflow-x-scroll scroll whitespace-nowrap scroll-smooth hide-scrollbar'>
+            {data.category7.map((item)=>(
+              <Card imagePath={item.img} name={item.name} description={item.description}></Card>
+            ))}
+          </div>
         </div>
-        <MdChevronRight size={60} onClick={(e)=>slideRight(e)} className='opacity-50 hover:opacity-100 cursor-pointer' />
+        <Buttons></Buttons>
       </div>
 
-      <h1 className='text-center mt-18 mb-6'>Sonstiges</h1>
-      <div className='relative flex items-center w-screen'>
-        <MdChevronLeft size={60} onClick={(e)=>slideLeft(e)} className='opacity-50 hover:opacity-100 cursor-pointer' />
-        <div id='slider1' className='slajder w-full h-full overflow-x-scroll scroll whitespace-nowrap scroll-smooth hide-scrollbar'>
-          {data.category10.map((item)=>(
-            <Card imagePath={item.img} name={item.name} description={item.description}></Card>
-          ))}
+      <div>
+        <h1 className='text-center mt-18 mb-6'>Sonstiges</h1>
+        <div className='relative flex items-center w-screen'>
+          <div id='slider1' className='slajder w-full h-full overflow-x-scroll scroll whitespace-nowrap scroll-smooth hide-scrollbar'>
+            {data.category10.map((item)=>(
+              <Card imagePath={item.img} name={item.name} description={item.description}></Card>
+            ))}
+          </div>
         </div>
-        <MdChevronRight size={60} onClick={(e)=>slideRight(e)} className='opacity-50 hover:opacity-100 cursor-pointer' />
+        <Buttons></Buttons>
       </div>
-
 
       <Footer></Footer>
     </>
---
+++ src/Buttons.jsx
@@ -0,0 +1,29 @@
+function Buttons(){
+    
+    const slideLeft = (e) => {
+        var slider = e.currentTarget.parentElement.parentElement.querySelector('div').querySelector('.slajder')
+        slider.scrollLeft -= 500
+    }
+
+    const slideRight = (e) => {
+        var slider = e.currentTarget.parentElement.parentElement.querySelector('div').querySelector('.slajder')
+        slider.scrollLeft += 500
+    }
+
+    return(
+        <div className="slider-with-title__nav flex items-center justify-center">
+          <button onClick={(e)=>slideLeft(e)} className="slider-with-title__prev m-1 w-12 h-12 border border-gray-400 rounded-full flex items-center justify-center bg-white hover:bg-purple-200 cursor-pointer" aria-label="Prev item">
+            <svg width="20" height="20" viewBox="0 0 20 20" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+              <path fill-rule="evenodd" clip-rule="evenodd" d="M7.20537 6.37206L3.87204 9.70539L3.57741 10L3.87204 10.2946L7.20537 13.628L7.79462 13.0387L5.17259 10.4167H17.0833V9.58335H5.17259L7.79462 6.96131L7.20537 6.37206Z"></path>
+            </svg>
+          </button> 
+          <button onClick={(e)=>slideRight(e)} className="slider-with-title__next m-1 w-12 h-12 border border-gray-400 rounded-full flex items-center justify-center bg-white hover:bg-purple-200 cursor-pointer" aria-label="Next item">
+            <svg width="20" height="20" viewBox="0 0 20 20" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+              <path fill-rule="evenodd" clip-rule="evenodd" d="M13.4554 6.37206L16.7887 9.70539L17.0833 10L16.7887 10.2946L13.4554 13.628L12.8661 13.0387L15.4881 10.4167H3.57739V9.58335H15.4881L12.8661 6.96131L13.4554 6.37206Z"></path>
+            </svg>
+          </button>
+        </div>
+    )
+}
+
+export default Buttons;
